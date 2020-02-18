cc3f6333b5285ec6443d4166f9a9efd5db8f494c
Victoria <viktoriag373@gmail.com>
2020-02-18 23:47:35 +0300

    1. Сделан адаптив страницы

--- js/script.js
+++ js/script.js
@@ -9,24 +9,22 @@ iconMenu.addEventListener('click', function() {
 	menuBody.classList.toggle('active');
 	document.body.classList.toggle('lock');
 })
-
-
-
-
-function ibg(){
-	document.querySelectorAll(".ibg").forEach(el => {
-		if(el.querySelector('img')){
-			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
-		}
-	});
-}
-
-ibg();
-
  
 
 
 $(document).ready(function(){
+
+	function ibg(){
+		$.each($('.ibg'), function(index, val) {
+			if($(this).find('img').length>0){
+				$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+			}
+		});
+	}
+
+	ibg();
+
+
 	$('.slider').slick({
 		arrow:true,
 		dots:true,

c617e26c9e7d14e39801478eddbc7245873068e4
Vika <viktoriag373@gmail.com>
2020-08-18 20:37:38 +0300

    1. Добавлен онлайн чат 2. Мелкие правки 3. Заменен слайдер на swiper

--- #sourse/js/script.js
+++ #sourse/js/script.js
@@ -1,87 +1,60 @@
 'use strict'
 
 $(document).ready(function(){
-	let isMenuOpen = false;
-	$('.icon-menu').click(function(event){
-		if (isMenuOpen === false) {
-			openBurger()
-		} else {
-			closedBurger()		
-		}
-		// toggleClassBurger();
-	})
+    let isMenuOpen = false;
+    $('.icon-menu').click(function(event){
+        if (isMenuOpen === false) {
+            openBurger()
+        } else {
+            closedBurger()
+        }
+    })
 
 
-	function openBurger() {
-		isMenuOpen = true;
-		$('.icon-menu,.menu__body').addClass('active');
-		$('body').addClass('lock');
-	}
+    function openBurger() {
+        isMenuOpen = true;
+        $('.icon-menu,.menu__body').addClass('active');
+        $('body').addClass('lock');
+    }
 
-	function closedBurger() {
-		isMenuOpen = false;
-		$('.icon-menu,.menu__body').removeClass('active');
-		$('body').removeClass('lock');		
-	}
-
-
+    function closedBurger() {
+        isMenuOpen = false;
+        $('.icon-menu,.menu__body').removeClass('active');
+        $('body').removeClass('lock');
+    }
 
 
-	// function toggleClassBurger(){
-	// 	$('.icon-menu,.menu__body').toggleClass('active');
-	// 	$('body').toggleClass('lock');
-	// }
+    let menuHeight = $('.header__row').height();
 
-
-	let menuHeight = $('.header__row').height();
-
-	$(function(){
+    $(function(){
         $("a[href^='#']").click(function(){
-        	// toggleClassBurger();
-        	closedBurger()
+
+            closedBurger()
             var _href = $(this).attr("href");
             $("html, body").animate({scrollTop: $(_href).offset().top-menuHeight+"px"});
             return false;
         });
-	});
+    });
 
-
-	// function ibg(){
-	// 	$.each($('.ibg'), function(index, val) {
-	// 		if($(this).find('img').length>0){
-	// 			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-	// 		}
-	// 	});
-	// }
-
-	// ibg();
-
-
-	$('.slider').slick({
-		arrow:true,
-		dots:true,
-		adaptiveHeight:true,
-		slidesToShow:2,
-		slidesToScroll:1,
-		speed:1000,
-		autoplay:false,
-		autoplaySpeed:2500,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
-	});
-
-
-
-
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: false,
+        trueslidesPerView: 1,
+        keyboard: {
+            enabled: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
 });
-    
+
 
 
 
